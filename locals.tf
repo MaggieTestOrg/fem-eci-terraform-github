4d5d04599df2a5c830b4b5f638f764e5b1a7981e
Maggie <maggie@nightshiftdevelopment.com>
2024-02-04 18:34:21 -0500

    add archived (#1)

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
     "fem-eci-terraform-tfe" = {
       description        = "Automation for Terraform Enterprise"
       gitignore_template = "Terraform"
+      archived           = true
       name               = "fem-eci-terraform-tfe"
       topics             = ["frontend-masters", "terraform"]
       visibility         = "public"
@@ -10,6 +11,7 @@ locals {
     "fem-eci-terraform-github" = {
       description        = "Automation for Github"
       gitignore_template = "Terraform"
+      archived           = true
       name               = "fem-eci-terraform-github"
       topics             = ["frontend-masters", "terraform"]
       visibility         = "public"
